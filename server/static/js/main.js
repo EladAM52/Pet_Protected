4cd8e6657d2a45ee59d3b87e2a44430db203e6e5
EladAM52 <93151656+EladAM52@users.noreply.github.com>
2021-12-19 14:55:41 +0200

    register

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -33,3 +33,21 @@ app.controller("loginCtrl", function ($scope, $http, $window) {
         });
     }
 });
+
+app.controller("registerCtrl", function ($scope, $http, $window) {
+    $scope.data = {};
+    $scope.error = '';
+
+    $scope.submit = function () {
+        $scope.error = '';
+        $http({
+            method: 'POST',
+            url: '/accounts/register',
+            data: $scope.data
+        }).then(function successCallback(response) {
+            $window.location.href = '/'; // redirect to home page
+        }, function errorCallback(response) {
+            $scope.error = response.data.message
+        });
+    }
+});
