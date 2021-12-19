fc6ebc3c79c26a65bee745e3ccbdf4a64d34b36d
Lin Bokobza <69593176+linbokobza@users.noreply.github.com>
2021-12-19 14:14:22 +0200

    Review & Favorite

--- server/share_place/models.py
+++ server/share_place/models.py
@@ -22,3 +22,20 @@ class Post(models.Model):
         return self.title
 
 
+
+class Review(models.Model):
+    Fullname = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('user', 'post'),)
