3f297b40b59cb68d5ee494dc080163a54245bd0c
EladAM52 <93151656+EladAM52@users.noreply.github.com>
2021-12-19 20:02:01 +0200

    directive func

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -344,3 +344,21 @@ app.controller("homeCtrl", function ($scope, $http) {
         });
     }
 });
+
+
+
+
+app.directive('file', function () {
+    return {
+        scope: {
+            file: '='
+        },
+        link: function (scope, el, attrs) {
+            el.bind('change', function (event) {
+                var file = event.target.files[0];
+                scope.file = file ? file : undefined;
+                scope.$apply();
+            });
+        }
+    };
+});
