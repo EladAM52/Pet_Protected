bfb5aef0b0ab168478645307fcea3c72c64c3d40
Lior Swissa <93181119+liorsw5@users.noreply.github.com>
2021-12-19 20:57:37 +0200

    edit class Favorite

--- server/share_place/models.py
+++ server/share_place/models.py
@@ -32,5 +32,10 @@ class Review(models.Model):
     def __str__(self):
         return self.title
 
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = (('user', 'post'),)
 
