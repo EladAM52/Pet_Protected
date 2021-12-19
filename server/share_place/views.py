d477df5c53415a63fb65326c9cd531b8a44dfe97
Lior Swissa <93181119+liorsw5@users.noreply.github.com>
2021-12-19 20:53:46 +0200

    edit delete post

--- server/share_place/views.py
+++ server/share_place/views.py
@@ -266,3 +266,30 @@ def edit_profile(request):
 
 @require_http_methods(['DELETE'])
 @login_required
+
+def delete_post(request, pk):
+    try:
+        post = Post.objects.get(id=pk)
+        if post.author != request.user and not request.user.is_superuser:
+            return JsonResponse(
+                data={"message": "Not permitted"},
+                status=401
+            )
+        post.delete()
+        return JsonResponse(
+            data={
+                "success": True
+            }
+        )
+    except ObjectDoesNotExist:
+        return JsonResponse(
+            data={
+                "message": "Post not found"
+            },
+            status=404
+        )
+
+
+@login_required
+@require_POST
+
