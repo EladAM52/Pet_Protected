762a750bd1a2d6cef6179213975d2b9344541b38
yarinshriker <94844404+yarinshriker@users.noreply.github.com>
2021-12-30 11:40:18 +0200

    test - Share Place View TestCase

--- server/share_place/tests.py
+++ server/share_place/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from .models import *
+from django.urls import reverse,resolve
+from .views import *
+import json
 
-# Create your tests here.
+
+class SharePlaceViewTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = User.objects.create_user(username="test", first_name='test', last_name='tester',
+                                             email='test@test.com', password='password')
+        self.user.profile.phone_number = '123'
+        self.user.save()
