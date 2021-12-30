2ed226b5cb38613a27707b3c2f92a7b09b08d6a1
EladAM52 <93151656+EladAM52@users.noreply.github.com>
2021-12-30 11:52:11 +0200

    test- test_register_user

--- server/accounts/tests.py
+++ server/accounts/tests.py
@@ -6,3 +6,25 @@ from django.contrib.auth.models import User
 class AccountsViewTestCase(TestCase):
     def setUp(self) -> None:
         self.client = Client()
+
+        
+    def test_register_user(self):
+        response = self.client.post('/accounts/register',
+                                    content_type="application/json",
+                                    data={
+                                        'email': "test@test.com",
+                                        'username': "test",
+                                        'first_name': 'first test',
+                                        'last_name': 'last test',
+                                        'password': 'password',
+                                        'phone_number': '111111111',
+                                    })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'success': True})
+        self.assertEqual(User.objects.count(), 2)  # admin + created user
+        created_user = User.objects.get(username="test")
+        self.assertEqual(created_user.email, "test@test.com")
+        self.assertEqual(created_user.first_name, "first test")
+        self.assertEqual(created_user.last_name, "last test")
+        self.assertEqual(created_user.profile.phone_number, '111111111')
+        self.assertTrue(created_user.is_authenticated)
