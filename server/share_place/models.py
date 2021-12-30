0c2ecb9e623b0bc31914f194769d048a52b8b2c7
yarinshriker <94844404+yarinshriker@users.noreply.github.com>
2021-12-30 12:13:20 +0200

    add Product modal

--- server/share_place/models.py
+++ server/share_place/models.py
@@ -7,7 +7,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
